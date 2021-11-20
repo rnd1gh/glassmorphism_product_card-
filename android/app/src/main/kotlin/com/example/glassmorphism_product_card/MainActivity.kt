72d35f9030537afd3d18f0bcaa7c8eb865cf43f0
Nguyen Phuoc Hau <nguyenphuochau.dev@gmail.com>
2021-11-20 09:37:29 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/glassmorphism_product_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.glassmorphism_product_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data.dart
@@ -0,0 +1,6 @@
+List<String> shoesImageUrls = [
+  'assets/shoes_0.png',
+  'assets/shoes_1.png',
+  'assets/shoes_2.png',
+  'assets/shoes_3.png'
+];
